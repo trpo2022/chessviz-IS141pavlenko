109e7abeb864bdcd592e0bc54debf1cf221fe727
Павленко Владислав Сергеевич <is141s11@bwp12.csc.local>
2022-04-22 15:25:51 +0700

    add .github/workflows/build.yml

---
+++ src/libchessviz/find_pos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <libchessviz/chess.h>
+
+
+int *find_pos(char *tmp, int *pos)
+{
+  int stolb = tmp[0]-'0'-48;
+  int stroka = tmp[1]-'0';
+  stroka = 8 - stroka;
+ 
+  pos[0] = stroka;
+  pos[1] = stolb;
+ 
+  return pos;
+}
