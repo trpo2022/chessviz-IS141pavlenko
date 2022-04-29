a18348ba29020664faee58b43602101523fcb05b
Павленко Владислав Сергеевич <is141s11@bwp01.csc.local>
2022-04-29 13:58:48 +0700

    apply clang-format

--- src/libchessviz/chess.h
+++ src/libchessviz/chess.h
@@ -2,6 +2,6 @@
 #define n 9
 #define str 18
 
-int *find_pos(char *tmp, int *pos);
-int check_mate(char *tmp);
-int move(int *early_pos, int *last_pos,int fl , char board[n][n]);
+int* find_pos(char* tmp, int* pos);
+int check_mate(char* tmp);
+int move(int* early_pos, int* last_pos, int fl, char board[n][n]);
