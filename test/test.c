f8636186e0888b4788280777c1980d77182c228d
IS141pavlenko <scccpavlenko@yandex.ru>
2022-05-14 18:26:47 +0700

    fix codestyle

--- test/test.c
+++ test/test.c
@@ -15,20 +15,20 @@ CTEST(chess_suite, test_check_mate)
 
 CTEST(chess_suite, test_check_mate2) 
 {
-	char tmp[] = {'e'};
-	int res; 
-	res = check_mate(tmp);		
+    char tmp[] = {'e'};
+    int res; 
+    res = check_mate(tmp);		
     const int exp = 0;
     ASSERT_EQUAL(exp, res);
 }
 
 CTEST(chess_suite, test_move) 
 {
-	char tmp[] = {'e', '2', '-', 'e', '4'};
-	int pos[N];
-	find_pos(tmp, pos);
-	int res = pos[0];
-	int exp = 6;
+    char tmp[] = {'e', '2', '-', 'e', '4'};
+    int pos[N];
+    find_pos(tmp, pos);
+    int res = pos[0];
+    int exp = 6;
     ASSERT_EQUAL(exp, res);
     exp = 5;
     res = pos[1];
